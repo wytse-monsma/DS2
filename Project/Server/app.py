d270230cc3942160746197cedaf0cff88e1a6508
Stefan Linux <stefan1752002@gmail.com>
2025-04-08 15:11:32 +0200

    Updated flask app, ready now

--- Project/Server/app.py
+++ Project/Server/app.py
@@ -28,4 +28,4 @@ def query():
         return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1717, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
